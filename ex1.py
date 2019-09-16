005850c714fe49d18846a0e17f1028beb9cbb974
ZY1N <zhang.yine@husky.neu.edu>
2019-09-15 22:39:31 -0700

    ddd

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! printing.')
+print("I'd much rather you 'not'.\n")
+print('I "said" do not touch this.')
